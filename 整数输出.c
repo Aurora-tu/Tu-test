9de93abce956d4aba6070647e853953879a4960f
Aurora-tu <3410214690@qq.com>
2024-10-29 22:02:45 +0800

    homework

---
+++ 整数输出.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	int a=10;
+	printf("a:%5d\n%7o\n%8x\n%9u\n",a,a,a,a);
+	printf("%d\n",sizeof(a));
+	int b=-10;
+	printf("b:%d\n%o\n%x\n%u\n",a,a,a,a);
+	int c=-100;
+	printf("%d\n%o\n",c,c);
+	double d=2.5;
+	printf("%-7.1f\n",d);
+	printf("%d\n",sizeof(d));
+	double a1=0.015+0.045;
+	double a2=0.05+0.01;
+	double a3=0.03+0.03;
+	double sum=0;
+	int i;
+	for(i=0;i<=10;i++){
+		sum+=0.01;
+		if
+	}
+	return 0;
+}
